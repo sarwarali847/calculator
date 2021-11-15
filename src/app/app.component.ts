0cd85f8c4cbb4a7c033c29b124df96ff0be403b0
sarwarali847 <sarwarali847@gmail.com>
2021-11-15 23:57:05 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,83 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculator';
+
+  subDisplayText = "";
+  mainDisplayText = "";
+  operand1: number; 
+  operand2: number; 
+  operator = ""; 
+  calculationString = '';
+  operatorSet = false; 
+
+  pressKey(key: string) {
+    if (key === '/' || key === 'x' || key === '-' || key === '+') {
+      const lastKey = this.mainDisplayText[this.mainDisplayText.length - 1];
+      if (lastKey === '/' || lastKey === 'x' || lastKey === '-' || lastKey === '+') {
+        this.operatorSet = true;
+      }
+      if ((this.operatorSet) || (this.mainDisplayText === "")) {
+        return null;
+      }
+      this.operand1 = parseFloat(this.mainDisplayText);
+      this.operator = key;
+      this.operatorSet = false;
+    }
+    if (this.mainDisplayText.length === 10) {
+      return null;
+    }
+    this.mainDisplayText += key;
+  }
+  allClear() {
+    this.mainDisplayText = '';
+    this.subDisplayText = '';
+    this.operatorSet = false;
+  }
+  getAnswer() {
+    this.calculationString = this.mainDisplayText;
+    this.operand2 = parseFloat(this.mainDisplayText.split(this.operator)[1]);
+    if (this.operator === '/') {
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 / this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'ERROR';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } 
+    
+    else if (this.operator === 'x') {
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 * this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'ERROR';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } 
+    
+    else if (this.operator === '-') {
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 - this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'NaN';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } 
+    
+    else if (this.operator === '+') {
+      this.subDisplayText = this.mainDisplayText;
+      this.mainDisplayText = (this.operand1 + this.operand2).toString();
+      this.subDisplayText = this.calculationString;
+      if (this.mainDisplayText.length > 9) {
+        this.mainDisplayText = 'NaN';
+        this.subDisplayText = 'Range Exceeded';
+      }
+    } 
+    
+    else {
+      this.subDisplayText = 'ERROR: Invalid Operation';
+    }
+  }
 }
